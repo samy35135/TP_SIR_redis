4298260984dc6293ba52f786bb6ad7445bfbdbb5
11002391 <11002391@e214m06.istic.univ-rennes1.fr>
2015-02-10 15:31:15 +0100

    Ajout de redis de nouveaux fichiers

---
+++ TP_SIR_redis/src/main/java/TP_SIR_redis/TP_SIR_redis/App.java
@@ -0,0 +1,13 @@
+package TP_SIR_redis.TP_SIR_redis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
